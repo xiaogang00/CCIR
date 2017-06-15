1aba26895f80af385b5c775b1d2a85fa470f47ec
xiaogang00 <872310734@qq.com>
2017-06-15 19:26:03 +0800

    remove dir

--- code/write.py
+++
@@ -1,9 +0,0 @@
-import os
-if __name__ == '__main__':
-    if not os.path.exists('./test'):
-        os.makedirs('./test')
-    file_object = open('./test/%d' %(1), 'w')
-    a = "answer number %d  %d  %lf  \n" % (1, 2, 0.8)
-    file_object.write(a)
-    a = "answer number %d  %d  %lf  \n" % (1, 2, 0.8)
-    file_object.write(a)
